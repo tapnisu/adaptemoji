d2864ca5141af68dc17517b06fedefcb5f6843a6
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-03-26 19:07:06 +0700

    Created `AdaptiveEmojiConvert` trait

--- src/lib.rs
+++ src/lib.rs
@@ -41,3 +41,39 @@ pub fn convert(img: &mut image::GrayAlphaImage, negative: bool) {
         }
     }
 }
+
+/// Convert your emoji into adaptive one
+pub trait AdaptiveEmojiConvert {
+    /// Converts the pixels of a GrayAlphaImage to negative if `negative` is true,
+    /// otherwise, converts them to positive.
+    ///
+    /// For Telegram's adaptive emojis, it doesn't matter, made to better show
+    /// what should you use depending on the background.
+    ///
+    /// # Arguments
+    ///
+    /// * `img` - A mutable reference to a GrayAlphaImage.
+    /// * `negative` - A boolean indicating whether to convert to negative or not.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use adaptemoji::AdaptiveEmojiConvert;
+    ///
+    /// # fn main() -> Result<(), image::ImageError> {
+    /// let mut img = image::open("./assets/examples/original.webp")?.to_luma_alpha8();
+    ///
+    /// let adaptive_img = img.convert(true);
+    /// # Ok(())
+    /// # }
+    /// ```
+    fn convert(&self, negative: bool) -> Self;
+}
+
+impl AdaptiveEmojiConvert for image::GrayAlphaImage {
+    fn convert(&self, negative: bool) -> image::GrayAlphaImage {
+        let mut img = self.clone();
+        convert(&mut img, negative);
+        img
+    }
+}
