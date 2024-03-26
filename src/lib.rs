4cb4ae13ef9ed27a9421bda3635ced19f0974764
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-03-26 17:58:46 +0700

    Added negative support

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 pub mod cli;
 
-pub fn convert(img: &mut image::GrayAlphaImage) {
+pub fn convert(img: &mut image::GrayAlphaImage, negative: bool) {
     for y in 0..img.height() {
         for x in 0..img.width() {
             let pixel = img.get_pixel(x, y);
-            let proc_pixel = image::LumaA::from([
-                255,
-                (((255 - pixel.0[0]) as f32 / 255f32) * pixel.0[1] as f32) as u8,
-            ]);
 
+            let new_alpha = (((if negative {
+                255 - pixel.0[0]
+            } else {
+                pixel.0[0]
+            }) as f32
+                / 255f32)
+                * pixel.0[1] as f32) as u8;
+
+            let proc_pixel = image::LumaA::from([255, new_alpha]);
             img.put_pixel(x, y, proc_pixel);
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
         .unwrap_or_else(|err| cmd.error(ErrorKind::InvalidValue, err).exit())
         .to_luma_alpha8();
 
-    adaptemoji::convert(&mut img);
+    adaptemoji::convert(&mut img, cli.negative);
 
     if let Err(err) = img.save(cli.output) {
         cmd.error(ErrorKind::Io, err).exit()
